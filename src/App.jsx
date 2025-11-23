b30afbe1c3e726361f459cd91339d3b2d60715ad
Oseias Dias <oseiasdias1953@gmail.com>
2025-11-24 02:52:28 +1300

    primeiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
-import 'react-toastify/dist/ReactToastify.css';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import AcessoOficina from "./page/pageAdmin/AcessoOficina.jsx";
-import HomeAdmin from './page/pageAdmin/HomeAdmin.jsx';
-import UsuariosApp from './page/pageAdmin/Utilizadores.jsx';
-import ListarLocal from './page/pageAdmin/ListarLocais.jsx';
-import Anuncios from './page/pageAdmin/ListarAnuncios.jsx';
-import ListarAdmin from './page/pageAdmin/ListarAdmin.jsx';
-
-import EditarFucionario from './page/pageAdmin/PageEditar/EditarPerfil.jsx';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
-
-
 import './App.css';
 
 
-const ProtectedRouteAdministrador = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem('token');
-  return isAuthenticated ? children : <Navigate to="/" />;
-};
 
 
 function App() {
@@ -32,83 +13,7 @@ function App() {
   return (
     <> 
     
-    <ToastContainer position="top-center" autoClose={3000} />
-
-      <Router>
-        <Routes>
-
-          <Route path="/" element={
-
-            <AcessoOficina />
-
-          } />
-
-          <Route
-            path="/homeAdmin/:email"
-            element={
-              <ProtectedRouteAdministrador>
-                <HomeAdmin />
-              </ProtectedRouteAdministrador>
-            }
-          />
-
-
-          <Route path="/pageTecnicos" element={
-            <ProtectedRouteAdministrador>
-              <UsuariosApp />
-            </ProtectedRouteAdministrador>
-
-          } />
-
-          <Route path="/pageAnuncios" element={
-            <ProtectedRouteAdministrador>
-              <Anuncios />
-            </ProtectedRouteAdministrador>
-
-
-
-          } />
-
-          {/**Routes de Add de Entidades */}
-          <Route path="/pageLocais" element={
-
-
-            <ProtectedRouteAdministrador>
-              <ListarLocal />
-            </ProtectedRouteAdministrador>
-
-
-          } />
-
-            {/**Routes de Add de Entidades */}
-          <Route path="/pageListarAdmin" element={
-
-
-            <ProtectedRouteAdministrador>
-              <ListarAdmin />
-            </ProtectedRouteAdministrador>
-
-
-          } />
-
-
-
-          {/**Routes de Add de Entidades */}
-          <Route path="pageEditPerfil/:email" element={
-
-            <ProtectedRouteAdministrador>
-              <EditarFucionario />
-            </ProtectedRouteAdministrador>
-
-          } />
-
-
-          {/**Routes de Add de Entidades */}
-
-
-
-        </Routes>
-      </Router>
+  
     </>
 
   );
